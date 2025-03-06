9e773d521e354c6ff1065f4bc3793e4b9fcd7b62
mireia11 <115173193+mireia11@users.noreply.github.com>
2025-03-06 18:37:02 +0100

    panells d'info afegits

--- Assets/Scripts/UI/Tutorial_UI.cs
+++ Assets/Scripts/UI/Tutorial_UI.cs
@@ -7,11 +7,20 @@ public class TutorialUI : MonoBehaviour
 {
     public RectTransform tutorialPanel;
     public RectTransform cogerObjetosPanel;
+    public RectTransform estrellasPanel;
+    public RectTransform runPanel;
+    public Canvas canvas;
 
     private Vector2 posicionInicialTutorial;
     private Vector2 posicionInicialCogerObjetos;
+    private Vector2 posicionInicialEstrellas;
+    private Vector2 posicionInicialRun;
+
     private float velocidad = 2f;  // velocitat de moviment
     private float amplitud = 10f;  // Ajusta què tant es mou de dalt a baix
+    private float DisappearTime = 10f;
+    private float estrellasVisibleTime = -1f;
+    private float runVisibleTime = -1f;
 
     void Start()
     {
@@ -24,25 +33,31 @@ public class TutorialUI : MonoBehaviour
         //Guardem posicio inical de cada panell
         posicionInicialTutorial = tutorialPanel.anchoredPosition;
         posicionInicialCogerObjetos = cogerObjetosPanel.anchoredPosition;
+        posicionInicialEstrellas = estrellasPanel.anchoredPosition;
+        posicionInicialRun = runPanel.anchoredPosition;
 
         tutorialPanel.gameObject.SetActive(true); // mostra tutorial al iniciar
         cogerObjetosPanel.gameObject.SetActive(true);
+        estrellasPanel.gameObject.SetActive(true);
+        runPanel.gameObject.SetActive(true);
 
     }
 
     void Update()
     {
 
-        if (tutorialPanel == null || cogerObjetosPanel == null) return;
+        if (tutorialPanel == null || cogerObjetosPanel == null || estrellasPanel == null || runPanel == null) return;
 
         float moviment = Mathf.Sin(Time.time * velocidad) * amplitud;
 
         //Movem panels
-        Vector2 vector2 = new(posicionInicialTutorial.x, posicionInicialTutorial.y + moviment);
-        tutorialPanel.anchoredPosition = vector2;
+        tutorialPanel.anchoredPosition = new Vector2(posicionInicialTutorial.x, posicionInicialTutorial.y + moviment);
         cogerObjetosPanel.anchoredPosition = new Vector2(posicionInicialCogerObjetos.x, posicionInicialCogerObjetos.y + moviment);
+        estrellasPanel.anchoredPosition = new Vector2(posicionInicialEstrellas.x, posicionInicialEstrellas.y + moviment);
+        runPanel.anchoredPosition = new Vector2(posicionInicialRun.x, posicionInicialRun.y + moviment);
 
-        if (Input.GetKeyDown(KeyCode.Space)) // Si pressionem espacio
+
+        if (Input.GetKeyDown(KeyCode.Space)) 
         {
             tutorialPanel.gameObject.SetActive(false); // Ocultem tutorial
         }
@@ -52,5 +67,55 @@ public class TutorialUI : MonoBehaviour
             cogerObjetosPanel.gameObject.SetActive(false);
         }
 
+        /*
+        CheckPanelVisibility(estrellasPanel, ref estrellasVisibleTime);
+
+        CheckPanelVisibility(runPanel, ref runVisibleTime);
+
+        //Comparem el temps actual Time.time amb el moment en el panell va ser vist
+        if (estrellasVisibleTime > 0 && Time.time - estrellasVisibleTime > DisappearTime)
+        {
+            estrellasPanel.gameObject.SetActive(false);
+            estrellasVisibleTime = -1f;
+        }
+
+        if (runVisibleTime > 0 && Time.time - runVisibleTime > DisappearTime)
+        {
+            runPanel.gameObject.SetActive(false);
+            runVisibleTime = -1f;
+        }
+        */
+
     }
+
+    /*
+     *  void CheckPanelVisibility(RectTransform panel, ref float visibleTime)
+    {
+        if (panel.gameObject.activeSelf && visibleTime < 0)
+        {
+            Vector3[] corners = new Vector3[4];
+            panel.GetWorldCorners(corners);
+            RectTransform canvasRect = canvas.GetComponent<RectTransform>();
+
+            bool isVisible = false;
+            foreach (Vector3 corner in corners)
+            {
+                if (RectTransformUtility.RectangleContainsScreenPoint(canvasRect, corner, Camera.main))
+                {
+                    isVisible = true;
+                    break;
+                }
+            }
+
+            if (isVisible)
+            {
+                visibleTime = Time.time;
+            }
+        }
+    }
+
+     
+     */
+
+
 }
